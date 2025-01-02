5be61e4a79de46b374489d2f23202ec492fb3be8
abadiet <titouan.abadie@tabadie.eu>
2025-01-02 23:34:02 +0100

    add resources
    update resources

--- src/view/renderer.cpp
+++ src/view/renderer.cpp
@@ -11,25 +11,48 @@ Renderer::Renderer(MTL::Device* device):
     buildRendererElements();
     buildDepthStencilState();
 
-/* main camera */
+    /* main camera */
     id = scene->addCamera();
     scene->getCamera(id)->setPosition({0.0f, 3.0f, 0.0f});
     scene->getCamera(id)->setOrientation({0.0f, 0.0f, 0.0f});
     scene->getCamera(id)->setProjection(3.14f / 8.0f, 4.0f / 3.0f, 0.1f, 20.0f);
 
-/* main light */
+    /* main light */
     id = scene->addLight();
     scene->getLight(id)->setPosition({0.0f, 10.0f, 0.0f});
     scene->getLight(id)->setColor({1.0f, 1.0f, 1.0f});
     scene->getLight(id)->setIntensity(80.0f);
 
-/* ground */
+    /* camera light */
+    id = scene->addLight();
+    scene->getLight(id)->setColor({1.0f, 1.0f, 1.0f});
+    scene->getLight(id)->setIntensity(3.0f);
+
+    /* moving lights */
+    id = scene->addLight();
+    scene->getLight(id)->setPosition({2.0f, 2.0f, 0.1f});
+    scene->getLight(id)->setColor({1.0f, 0.0f, 0.0f});
+    scene->getLight(id)->setIntensity(3.0f);
+
+    id = scene->addLight();
+    scene->getLight(id)->setPosition({-2.0f, -5.0f, 0.1f});
+    scene->getLight(id)->setColor({0.0f, 1.0f, 0.0f});
+    scene->getLight(id)->setIntensity(3.0f);
+
+    id = scene->addLight();
+    scene->getLight(id)->setPosition({3.0f, 0.0f, 0.1f});
+    scene->getLight(id)->setColor({0.0f, 0.0f, 1.0f});
+    scene->getLight(id)->setIntensity(3.0f);
+
+    /* ground */
     id = scene->addElement();
     scene->getElement(id)->setRendererElement(&elements[0]);
     scene->getElement(id)->setPosition({0.0f, 0.0f, 0.0f});
     scene->getElement(id)->setOrientation({1.57f, 0.0f, 0.0f});
     scene->getElement(id)->setScale({20.0f, 20.0f, 20.0f});
 
+
+    /* cubes */
     id = scene->addElement();
     scene->getElement(id)->setRendererElement(&elements[1]);
     scene->getElement(id)->setPosition({1.0f, 2.0f, 3.0f});
@@ -37,15 +60,17 @@ Renderer::Renderer(MTL::Device* device):
 
     id = scene->addElement();
     scene->getElement(id)->setRendererElement(&elements[1]);
-scene->getElement(id)->setPosition({1.0f, 2.0f, 4.0f});
+    scene->getElement(id)->setPosition({1.0f, 2.0f, 4.0f});
     scene->getElement(id)->setOrientation({0.0f, 2.0f, 0.5f});
     scene->getElement(id)->setScale({0.05f, 0.05f, 0.05f});
-    
-    // id = scene->addElement();
-// scene->getElement(id)->setRendererElement(&elements[1]);
-// scene->getElement(id)->setPosition({1.0f, 3.0f, 3.0f});
-// scene->getElement(id)->setScale({0.05f, 0.05f, 0.05f});
 
+    id = scene->addElement();
+    scene->getElement(id)->setRendererElement(&elements[1]);
+    scene->getElement(id)->setPosition({1.0f, 3.0f, 3.0f});
+    scene->getElement(id)->setScale({0.05f, 0.05f, 0.05f});
+
+    /* init */
+    scene->getCamera(0)->lookAt(*scene->getElement(1));
 
     scene->updateLights();
 }
@@ -65,6 +90,13 @@ void Renderer::buildRendererElements() {
     /* ground (actually just a colored cube) */
     elem = RendererElement::BuildSquare(device);
     elem.setColors({simd::float3{0.5f, 0.5f, 0.5f}});
+    elem.setTexture(Texture::Build(device, "../textures/20x20grid.png"));
+    elem.setTextureCoords({
+        simd::float2{0.0f, 0.0f},
+        simd::float2{1.0f, 0.0f},
+        simd::float2{1.0f, 1.0f},
+        simd::float2{0.0f, 1.0f}
+    });
     elem.update(device);
     elements.push_back(elem);
 
@@ -154,16 +186,20 @@ void Renderer::drawInMTKView(MTK::View* view) {
     encoder = commandBuffer->renderCommandEncoder(renderPass);
 
     /* CAMERA */
+    scene->getCamera(0)->mvmtCircle({1.0f, 3.0f, 3.0f}, {0.0f, 1.0f, 0.0f}, 0.02f, true);
     // scene->getCamera(0)->lookAt(*scene->getElement(1));
 
     /* LIGHTS */
-    // scene->getLight(1)->mvmtCircle({0.0f, 4.5f, 3.0f}, {0.0f, -1.0f, 0.0f}, 0.1f);
-    // scene->updateLight(1);
+    scene->getLight(1)->setPosition(scene->getCamera(0)->getPosition());
+    scene->getLight(2)->mvmtCircle({3.0f, 3.0f, 0.1f}, {0.0f, 1.0f, 0.0f}, 0.05f);
+    scene->getLight(3)->mvmtCircle({-2.0f, -3.0f, 0.1f}, {0.0f, 1.0f, 0.0f}, 0.05f);
+    scene->getLight(4)->mvmtCircle({5.0f, 2.0f, 0.1f}, {0.0f, 1.0f, 0.0f}, 0.05f);
+    scene->updateLights();
 
     /* ELEMENTS */
-    // scene->getElement(1)->lookAt(*scene->getElement(2), simd::float3{0.0f, 0.0f, 1.0f});//, simd::float3{0.0f, 1.0f, 0.0f}, simd::float3{0.0f, 1.0f, 0.0f});
-    scene->getElement(2)->mvmtCircle({1.0f, 2.0f, 3.0f}, {1.0f, 1.0f, 0.0f}, 0.02f);
-    // scene->getElement(3)->mvmtCircle({1.0f, 2.0f, 3.0f}, {0.0f, 0.4f, 0.3f}, 0.03f);
+    // scene->getElement(1)->lookAt(*scene->getElement(2), simd::float3{0.0f, 0.0f, 1.0f}, simd::float3{0.0f, 1.0f, 0.0f}, simd::float3{0.0f, 1.0f, 0.0f});
+    scene->getElement(2)->mvmtCircle({1.0f, 2.0f, 3.0f}, {1.0f, 1.0f, 0.0f}, 0.02f, true);
+    scene->getElement(3)->mvmtCircle({1.0f, 2.0f, 3.0f}, {0.0f, 0.4f, 0.3f}, 0.03f);
 
     /* DRAWING */
     /* mode */
@@ -184,7 +220,7 @@ void Renderer::drawInMTKView(MTK::View* view) {
         encoder->setVertexBuffer(rendererElement->getVertexBuffer(), 0, 0);
         encoder->setVertexBuffer(element->getBufferPositionTransform(), 0, 1);
         encoder->setVertexBuffer(element->getBufferNormalTransform(), 0, 2);
-encoder->setFragmentTexture(rendererElement->getTexture(), 0);
+        encoder->setFragmentTexture(rendererElement->getTexture(), 0);
         encoder->drawIndexedPrimitives(
             MTL::PrimitiveType::PrimitiveTypeTriangle,
             rendererElement->getIndices().size(),
